9870d7fc23680f865a37264a2dff5516a9a69fe9
battistaennio <ennio.battista99@gmail.com>
2024-10-01 16:46:49 +0200

    fix: error message

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -23,7 +23,7 @@ export default {
             })
             .catch(error => {
 
-                console.log(error);
+                console.log('Errore: ' + error.message);
             })
         },
     },
