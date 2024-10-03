4e4dc964171ef431886b7c8195d3e7c9bf359d8c
battistaennio <ennio.battista99@gmail.com>
2024-10-03 14:51:28 +0200

    fix: loader style

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -230,34 +230,4 @@ export default {
             }
         }
     }
-
-
-    #loader-container{
-
-        margin: 0 auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-
-        .loader {
-            width: 48px;
-            height: 48px;
-            border-radius: 50%;
-            display: inline-block;
-            border-top: 3px solid #000;
-            border-right: 3px solid transparent;
-            box-sizing: border-box;
-            animation: rotation 1s linear infinite;
-        }
-
-        @keyframes rotation {
-            0% {
-                transform: rotate(0deg);
-            }
-            100% {
-                transform: rotate(360deg);
-            }
-        } 
-    }
 </style>
